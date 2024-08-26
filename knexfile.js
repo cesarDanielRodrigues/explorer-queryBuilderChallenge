fa60b98a3ba0a2d297c7bbfe05f6ba9a305fc40f
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-08-26 07:04:58 -0300

    Fazendo conexão com o banco e criando a tabela users nele

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,17 @@
-// Update with your config settings.
+const path = require("path")
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    pool:{
+      afterCreate: (conn, cb)=> conn.run("  PRAGMA foreign_keys = ON", cb)
+    },
+    migrations:{
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
+    useNullAsDefault: true
   },
 };

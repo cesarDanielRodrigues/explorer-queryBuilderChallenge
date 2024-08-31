7211f56df6c95caa4217f5b255926865fb4b01e3
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-08-31 12:59:34 -0300

    Finalizando com a rota get para listar todas as tags presente dentro da table tag

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const Router = require("express")
 
 const usersRoutes = require("../routes/users.routes")
 const moviesNotesRoutes = require("./moviesNotes.routes")
+const tagsRoutes = require("./tags.routes")
 
 const route = Router()
 
 route.use("/users", usersRoutes)
 route.use("/moviesNotes", moviesNotesRoutes)
+route.use("/tags", tagsRoutes)
 
 module.exports = route
